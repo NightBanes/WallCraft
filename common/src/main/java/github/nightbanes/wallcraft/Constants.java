47695712297bd88042e5e642cb62a8d0fc44dd98
NightBane <jzimmerman4444@gmail.com>
2026-07-03 17:15:21 -0600

    Initial commit

---
+++ common/src/main/java/github/nightbanes/wallcraft/Constants.java
@@ -0,0 +1,11 @@
+package github.nightbanes.wallcraft;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "wallcraft";
+    public static final String MOD_NAME = "WallCraft";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
